377a8b461216f37a69c63d0b9d1d53c302291506
mo3az96 <mo3az.abdellatif@gmail.com>
2023-10-22 11:29:08 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  lazyLoad();
+
+  $(".menu-btn").on("click", function (e) {
+    $(".navbar").fadeIn(300);
+    $(".overlay").fadeIn(300);
+    $(".header-nav").addClass("active");
+    $("body").addClass("overflow");
+  });
+  $(".overlay,.close-btn").on("click", function (e) {
+    $(".navbar").fadeOut(300);
+    $(".overlay").fadeOut(300);
+    $(".header-nav").removeClass("active");
+    $("body").removeClass("overflow");
+  });
+
+  if ($(window).width() <= 767) {
+    $(".footer-title").click(function () {
+      $(".footer-title").not(this).removeClass("active");
+      $(this).toggleClass("active");
+      if ($(this).siblings().css("display") == "none") {
+        $(this).siblings().slideDown(500);
+      } else {
+        $(this).siblings().slideUp(500);
+      }
+      $(".footer-title").not(this).siblings().slideUp(500);
+    });
+  }
+});
